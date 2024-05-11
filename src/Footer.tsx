91f01c8565163e340badde5821bab5e650612948
Suzuka Takeuchi <sztk1@nifty.com>
2024-05-11 20:08:31 +0900

    ファイル変更

--- src/Footer.tsx
+++
@@ -1,9 +0,0 @@
-const Footer = () => {
-  return (
-    <footer className="footer" role="contentinfo" aria-label="フッター">
-      <p className="text-xs">&copy;2024 Suzunatsu demosite</p>
-    </footer>
-  );
-};
-
-export default Footer;
